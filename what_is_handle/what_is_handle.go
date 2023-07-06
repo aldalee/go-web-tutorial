da181a91089c03b51c3fb81f2e9be74208461d29
aldalee <hadoop_2020@163.com>
2023-07-06 15:03:40 +0800

    处理Handle请求

--- what_is_handle/what_is_handle.go
+++ what_is_handle/what_is_handle.go
@@ -2,10 +2,36 @@ package main
 
 import "net/http"
 
+type indexHandler struct{}
+
+func (ih *indexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Index!"))
+}
+
+type aboutHandler struct{}
+
+func (ah *aboutHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("About!"))
+}
+
+func welcome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome!"))
+}
+
 func main() {
+	ih := indexHandler{}
+	ah := aboutHandler{}
 	server := http.Server{
 		Addr:    "localhost:8080",
-		Handler: nil,
+		Handler: nil, // DefaultServeMux
+		//Handler: http.FileServer(http.Dir("wwwroot")), // http.FileServer
 	}
+	// 处理多个Handler
+	http.Handle("/index", &ih)
+	http.Handle("/about", &ah)
+	http.Handle("/welcome", http.HandlerFunc(welcome))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
 	server.ListenAndServe()
 }
